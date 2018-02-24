491befbf4c48973edffffdf96401dd232531501d
Krystine Carrington <krystinecarrington@gmail.com>
2018-02-23 18:15:54 -0800

    cleaned up code and comments.

--- FeatherM0_Storage/FeatherM0_testingSPI/main.c
+++ FeatherM0_Storage/FeatherM0_testingSPI/main.c
@@ -1,16 +1,21 @@
 #include <atmel_start.h>
 
-#define BUFFER_SIZE (50)                          //maximum input/output buffer transfer size for SPI
+#define BUFFER_SIZE (50)                            //maximum input/output buffer transfer size for SPI
 
 int main(void)
 {
+    /* CONSTANT DECLARATIONS */ 
+    uint8_t RESET[1]    = {0xFF};
+    uint8_t GET_FEAT[2] = {0x0F, 0xC0};
+    uint8_t SET_WEL[1]  = {0x06};
+    
     /* VARIABLE DECLARATIONS */
     const    uint8_t BUSY_MASK      = 0x01;         //mask for checking if the flash is busy
     const    uint8_t PROG_FAIL      = 0b00001000;   //mask for checking if the memory was programmed successfully
     const    uint8_t WEL_BIT        = 0x02;         //mask for checking if write enable is high
     volatile uint32_t spi_result    = 0;            //how many bytes were transfered during SPI communication. -1 if error
-    volatile uint8_t  MOSI[BUFFER_SIZE];            //master's output buffer 
-    volatile uint8_t  MISO[BUFFER_SIZE];            //master's input buffer
+    uint8_t  MOSI[BUFFER_SIZE];                     //master's output buffer 
+    uint8_t  MISO[BUFFER_SIZE];                     //master's input buffer
     struct   spi_xfer spi_buff;                     //SPI transfer descriptor
     int      i;                                     //loop control variable
     
@@ -27,7 +32,8 @@ int main(void)
         MOSI[i] = 0x00;  
     }
 
-    /* Associate flash buffers with SPI device */
+    /* Associate flash buffers with SPI device and set
+     * buffer size. */
     spi_buff.size  = BUFFER_SIZE;
     spi_buff.rxbuf = MISO;
     spi_buff.txbuf = MOSI;
@@ -38,12 +44,16 @@ int main(void)
 	spi_m_sync_enable(&SPI_0);
     
     /* Allow time for initial device setup before sending
-     * reset signal. Minimum time is 250us*/
+     * reset signal. Minimum time is 250us. */
     delay_ms(1);
-
-    /* Issue the RESET command: 0xFF. 
-     * Put command in MOSI buffer.  */
-    uint8_t RESET[1] = {0xFF};
+    
+    /**********************
+     * RESET SLAVE DEVICE *
+     **********************/ 
+    /* Issue the RESET command: 0xFF. Device reset on startup
+     * is not mandatory for this part, but is recommended. This
+     * also ensures previous run settings are reset within the 
+     * device. */
     MOSI[0] = RESET[0];
 
     /* Set slave select (CS) active low to communicate. */
@@ -57,12 +67,14 @@ int main(void)
     
     /* Minimum 1.25ms delay after reset command before any
      * other commands can be issued. Rounded up to 2ms. */
-    //delay_ms(200);
+    delay_ms(200);
     
+    /*************************
+     * CHECK STATUS REGISTER *
+     *************************/ 
     /* Issue the GET FEATURES command: 0x0F 
      * Send address of FEATURES register: 0xC0
      * Put command in MOSI buffer.  */
-    uint8_t GET_FEAT[2] = {0x0F, 0xC0};
     MOSI[0] = GET_FEAT[0];
     MOSI[1] = GET_FEAT[1];
 
@@ -74,36 +86,22 @@ int main(void)
 
     /* Read/write over SPI */
     gpio_set_pin_level(GPIO_PIN(CS),true);
-    
-    /* Waiting to see if this helps before issuing next command... 
-     * Sending GET FEATURES message again. */
-    delay_ms(500);
-     
-    /* Set slave select (CS) active low to communicate. */
-    gpio_set_pin_level(GPIO_PIN(CS),false);
 
-    /* Read/write over SPI */
-    spi_result = spi_m_sync_transfer(&SPI_0, &spi_buff);
-
-    /* Read/write over SPI */
-    gpio_set_pin_level(GPIO_PIN(CS),true);
-    
+    /********************
+     * SET WRITE ENABLE *
+     ********************/ 
     /* Issue the SET WEL BIT command to enable writing: 0x06
      * Issue GET FEATURES command and register address: 0x0FC0 */
-    uint8_t SET_WEL_GET_FEAT[3] = {0x06, 0x0F, 0xC0};
-    MOSI[0] = SET_WEL_GET_FEAT[0];
+    MOSI[0] = SET_WEL[0];
     MOSI[1] = 0;
 	
-	spi_buff.size  = 1;
+    /* In order for the chip to recognize the SET WEL BIT command, 
+     * ONLY one byte of data can be sent over the SPI connection
+     * containing the command. The status is checked immediately
+     * after in a different SPI transaction. */
+    delay_ms(500);
+	spi_buff.size = 1;
 	
-	/* Initialize input and output buffers */
-    for(i = 0; i < BUFFER_SIZE; i++)
-    { 
-        MISO[i] = 0xFF;
-    }
-    
-	delay_ms(500);
-		
     /* Set slave select (CS) active low to communicate. */
     gpio_set_pin_level(GPIO_PIN(CS),false);
     
@@ -113,19 +111,16 @@ int main(void)
     /* Read/write over SPI */
     gpio_set_pin_level(GPIO_PIN(CS),true);
 
-    /* Send GET FEATURES command again without setting the WEL bit */
-    MOSI[0] = 0;
-	
-	delay_ms(500);
-	MOSI[0] = SET_WEL_GET_FEAT[1];
-	MOSI[1] = SET_WEL_GET_FEAT[2];
+    /*************************
+     * CHECK STATUS REGISTER *
+     *************************/ 
+    /* Send GET FEATURES command to check the status of the 
+     * WEL bit. Also, reset the SPI buffer to its initial
+     * size. */
+    delay_ms(500);
+    MOSI[0] = GET_FEAT[0];
+    MOSI[1] = GET_FEAT[1];
 	spi_buff.size  = BUFFER_SIZE;
-	
-	/* Initialize input and output buffers */
-	//for(i = 0; i < BUFFER_SIZE; i++)
-	//{
-	//	MISO[i] = 0xFF;
-	//}
 
     /* Set slave select (CS) active low to communicate. */
     gpio_set_pin_level(GPIO_PIN(CS),false);
