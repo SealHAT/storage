a1c889d80260796ea6972fc1dee5f9dbc6dbcfd1
Krystine Carrington <krystinecarrington@gmail.com>
2018-04-16 17:04:52 -0700

    adding block comments for new functions

--- flash_io.h
+++ flash_io.h
@@ -20,8 +20,47 @@ typedef struct
     bool    active_descriptor;
 } FLASH_DESCRIPTOR;
 
+/*************************************************************
+ * FUNCTION: flash_io_read()
+ * -----------------------------------------------------------
+ * This function reads 
+ *
+ * Parameters:
+ *      fd      :   File descriptor for IO buffers.
+ *      buf     :   Buffer to return read bytes in.
+ *      count   :   Number of bytes to read. 
+ *
+ * Returns:
+ *      size    :   Number of bytes actually read. 
+ *************************************************************/
 ssize_t flash_io_read(FLASH_DESCRIPTOR fd, void *buf, size_t count);
+
+/*************************************************************
+ * FUNCTION: flash_io_write()
+ * -----------------------------------------------------------
+ * This function 
+ *
+ * Parameters:
+ *      fd      :   File descriptor for IO buffers.
+ *      buf     :   Buffer containing bytes to write.
+ *      count   :   Number of bytes to write. 
+ *
+ * Returns:
+ *      size    :   Number of bytes actually written. 
+ *************************************************************/
 ssize_t flash_io_write(FLASH_DESCRIPTOR fd, void *buf, size_t count);
-bool    flash_io_is_busy();
+
+/*************************************************************
+ * FUNCTION: flash_io_is_busy()
+ * -----------------------------------------------------------
+ * This function returns true if the flash device is currently
+ * busy. It returns false if the device is currently not busy.
+ *
+ * Parameters: none
+ *
+ * Returns:
+ *      isBusy    :   Returns TRUE if flash is busy.
+ *************************************************************/
+bool flash_io_is_busy();
 
 #endif /* FLASH_IO_H_ */
